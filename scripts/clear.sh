d50d1e8236e1b80ad21b81f8fb0dc51e7f11f47d
Defmc <defmc0101@gmail.com>
2021-09-25 12:36:42 -0400

    Update mdf

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -1,4 +1,18 @@
+DIR=$(pwd)
+cd ~
 doas paccache -r
 doas pacman -Rns $(pacman -Qtdq)
-doas rm -rf ~/.cache
+doas rm -rf .cache \
+	.zsh_history \
+	.zshrc-back \
+	.xsession-errors \
+	.xsession-errors.old \
+	.zshenv \
+	.profile \
+	.python_history \
+	.mplayer \
+	.gnuplot_history \
+	.calc_history
+
 python3 'scripts/clear.py'
+cd $DIR
