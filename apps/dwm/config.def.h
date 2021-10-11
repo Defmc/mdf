f010080f7b85a7c7d4fd03f634577b3df2bd3eec
Defmc <defmc0101@gmail.com>
2021-10-10 23:02:13 -0400

    Update mdf

--- apps/dwm/config.def.h
+++ apps/dwm/config.def.h
@@ -61,11 +61,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *flamecmd[] = { "flameshot", "&", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd  } },
+        { MODKEY,                       XK_Print,  spawn,          {.v = flamecmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
