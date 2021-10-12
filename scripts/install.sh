24fcafef2251f90469704d52d820a3887a23c1fe
Defmc <defmc0101@gmail.com>
2021-10-11 23:29:33 -0400

    Update mdf

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,14 +9,19 @@ actual_dir="$(pwd)"
 
 git clone https://github.com/Defmc/mdf mdf
 
+pprint "Copying files..." $red_col
 #cp -rf mdf/* ~
+pprint " Done!\n" $green_col
 
 for dir in ~/apps/*/; do
     cd $dir
     pprint "Compiling $dir..." $red_col
     ./build.sh &> /dev/null
-    pprint " Builded!" $green_col
+    pprint " Builded!\n" $green_col
 done
 
+pprint "Removing temp files..." $red_col
 rm -rf ~/.git
+pprint " Done!\n" $green_col
 cd $actual_dir
+pprint "Bodia amdrade gostosa\n" $green_col
