b9fb9f0a3b542bf9f105eacfc3f8d5bb32de3245
Defmc <defmc0101@gmail.com>
2021-10-11 23:23:37 -0400

    Update mdf

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,7 @@ git clone https://github.com/Defmc/mdf mdf
 for dir in ~/apps/*/; do
     cd $dir
     printf "\e[31m\e[1mCompiling $dir...\e[0m\e[39m\n\n"
-    ./build.sh
+    ./build.sh &> /dev/null
     printf "\n\e[32m\e[1mBuild completed!\e[0m\e[39m\n\b"
 done
 
