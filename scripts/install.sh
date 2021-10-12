970e61be2c5e12c35c96a936ac92b739fee4fe0b
Defmc <defmc0101@gmail.com>
2021-10-11 23:14:05 -0400

    Update mdf

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,14 @@
-actual_dir = cwd
+actual_dir=cwd
 
 git clone https://github.com/Defmc/mdf mdf
 
-cp -rf mdf/* ~
+#cp -rf mdf/* ~
 
 for dir in ~/apps/*/; do
     cd $dir
     echo "\e[97m\e[1mCompiling $dir...\e[0m\e[39m"
     ./build.sh
+    echo "\e[32m\e[1mBuild completed\e[0m\e[39m"
 done
 
 rm -rf ~/.git
