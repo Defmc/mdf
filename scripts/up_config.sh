341d8597dfb69a6b1afbee813b57cc7c40e5b62c
Defmc <defmc0101@gmail.com>
2021-10-30 13:15:34 -0400

    Update mdf

--- scripts/up_config.sh
+++ scripts/up_config.sh
@@ -1,7 +1,3 @@
-load_colors(){
-	source ~/scripts/colors.sh
-}
-
 rm_buff(){
 	printf "$FG_RED Clearing buffer...\n"
 	mv ~/dev/mdf ~/dev/mdf-old
@@ -10,14 +6,14 @@ rm_buff(){
 	rm -rf ~/dev/mdf-old
 }
 
-upF(){
+up_file(){
 	printf "$FG_WHITE Copying ~/$1/$2..."
 	mkdir -p ~/dev/mdf/$1
 	cp -r ~/$1/$2 ~/dev/mdf/$1/$2
 	printf "$FG_GREEN Ok!\n"
 }
 
-rootUpF(){
+root_up_file(){
 	printf "$FG_WHITE Copying /$1/$2..."
 	mkdir -p ~/dev/mdf/$1
 	cp -r /$1/$2 ~/dev/mdf/$1/$2
@@ -25,36 +21,32 @@ rootUpF(){
 }
 
 up_files(){
-	upF . .Xresources
-	upF . .xinitrc
+    up_file . .Xresources
+    up_file . .xinitrc
+    up_file .icons fuchsia
+    up_file apps/dwm .
+    up_file apps/st .
+    up_file apps/slstatus .
+    up_file .config picom.conf
+    up_file . .zshrc
+    up_file .config/nvim init.lua
+    up_file scripts .
 
-
-	upF .icons fuchsia
-	rootUpF usr/share/themes onedark
-
-	upF apps/dwm .
-	upF apps/st .
-	upF apps/slstatus .
-
-	upF .config picom.conf
-
-	upF . .zshrc
-	upF .config/nvim init.lua
-	upF scripts .
+    root_up_file usr/share/themes onedark
 }
 
 up_git(){
 	OLDDIR=$(pwd)
 	cd ~/dev/mdf
-	printf "$FG_WHITE Commiting... $RESET"
+	printf "$FG_WHITE" "Commiting...$RESET"
 	git add --all
 	git commit -am "Update mdf"
 	git push origin main
 	cd $OLDDIR
-	printf "$FG_GREEN Upload completed $RESET\n"
+	printf "$FG_GREEN" "Upload completed $RESET\n"
 }
 
-load_colors
+source ~/scripts/colors.sh
 rm_buff
 up_files
 up_git
