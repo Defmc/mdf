b129cab1d8b23ce58469eace27b8d85f7fc8109c
Defmc <defmc0101@gmail.com>
2021-11-01 20:52:30 -0400

    Update mdf

--- apps/dwm/build.sh
+++
@@ -1,9 +0,0 @@
-source ~/scripts/colors.sh
-source  ~/scripts/autorun/prog.sh
-
-printf "$FG_RED Compiling...\n$RESET"
-doas make clean install
-printf "$FG_RED Optimizing binary...\n$RESET"
-opt dwm
-printf "$FG_RED Clearing temporaries configs...\n$RESET"
-rm -f config.h
---
+++ scripts/ui_tools.sh
@@ -0,0 +1,14 @@
+source ~/scripts/colors.sh
+
+# Show execution state of a command
+# Args:
+#       Command
+#       Message to be printed
+log_proc() {
+    $=1 1>/dev/null 2>&1
+    if [ $? = 0 ]; then
+        printf "\t$FG_GREEN%s$RESET %s...\n" "[Ok]" $2
+    else
+        printf "\t$FG_RED%s$RESET %s...\n" "[Er]" $2
+    fi
+}
