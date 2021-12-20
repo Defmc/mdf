005c87e60e1c16315efd6644844866e67ab60ce0
Defmc <defmc0101@gmail.com>
2021-12-20 16:08:09 -0400

    Update mdf

--- apps/build.sh
+++
@@ -1,7 +0,0 @@
-source ~/scripts/autorun/prog.sh
-source ~/scripts/ui_tools.sh
-
-log_proc "doas make clean install" "Compiling"
-log_proc "doas strip /usr/local/bin/$1 && doas upx /usr/local/bin/$1" "Optimizing binary"
-log_proc "rm -rf config.h" "Clearing temporaries configs"
-rm -f config.h
