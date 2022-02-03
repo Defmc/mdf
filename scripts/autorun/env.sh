9b9a8285e35c3848e3645841cb3ca72f746ee969
Defmc <defmc0101@gmail.com>
2022-02-02 20:44:12 -0400

    Update mdf

--- scripts/autorun/env.sh
+++ scripts/autorun/env.sh
@@ -1,4 +1,6 @@
+source ~/.cargo/env
+
 export EDITOR=nvim
-#export RUSTC_WRAPPER=/home/me/.cargo/bin/cachepot
-export CACHEPOT_LOG=debug
-export RUSTC_FLAGS='-C link-arg=-fuse-ld=lld'
+export RUSTC_WRAPPER=/home/me/.cargo/bin/cachepot
+#export CACHEPOT_LOG=debug
+#export RUSTC_FLAGS='-C link-arg=-fuse-ld=lld'
--- scripts/clear_cache.sh
+++ scripts/clear_cache.sh
@@ -7,8 +7,12 @@ log_proc "doas paccache -r" "Clearing pacman cache"
 log_proc "paru --clean --noconfirm" "Clearing aur cache"
 
 PKGS=$(pacman -Qtdq)
-log_proc "[ ! -z $PKGS ] && doas pacman -Rns --noconfirm $PKGS" "Removing breaked dependences"
+if [ ! $PKGS -z ]; then
+    echo $PKGS
+    log_proc "[ ! -z $PKGS ] && doas pacman -Rns --noconfirm $PKGS" "Removing breaked dependences"
+fi
 
-log_proc "rm -rf $(xargs -a ~/scripts/clear_cache_targets.txt)" "Removing cache folders"
+rm -rf $(xargs -a ~/scripts/clear_cache_targets.txt | cat)
+# "Removing cache folders"
 
 cd $DIR
