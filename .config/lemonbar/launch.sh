d7b016dfaecd89996d884912f9530d3c0cee2eb7
Defmc <https://Defmc@github.com>
2022-02-26 02:02:45 -0400

    Update mdf

---
+++ .config/lemonbar/launch.sh
@@ -0,0 +1,1 @@
+~/.config/lemonbar/config.sh | lemonbar -p -f "Jetbrains Mono:style=Bold" -f "Jetbrains Mono:style=Bold"
--- scripts/autorun/env.sh
+++ scripts/autorun/env.sh
@@ -1,6 +1,4 @@
 source ~/.cargo/env
 
 export EDITOR=nvim
-export RUSTC_WRAPPER=/home/me/.cargo/bin/cachepot
-export RUSTC_FLAGS='-C linker-flavor=mold'
-alias rustlint="cargo clippy -- -A clippy::pedantic -A clippy::nursery -A clippy::cargo"
+
