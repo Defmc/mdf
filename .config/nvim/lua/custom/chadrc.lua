7eeabf5e2b2eb4b519458e909c4753f5217f56f8
Defmc <https://Defmc@github.com>
2022-03-20 21:14:32 -0400

    Update mdf

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -7,35 +7,25 @@ map("n", "<C-S-Down>", ":m+1<CR>")
 map("n", "<A-Left>", ":vertical resize -5<CR>")
 map("n", "<A-Right>", ":vertical resize +5<CR>")
 map("t", "<esc>", "<C-\\><C-n>")
+map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
 
 M.ui = {
-   theme = "tokyonight",
+  theme = "tokyonight",
+  italic_comments = true
 }
 
 M.plugins = {
   install = require "custom.plugins",
+  status = {
+    colorizer = true
+  },
   options = {
       lspconfig = {
          setup_lspconf = "custom.plugins.lspconfig",
       },
    },
 }
-
-require ("custom.plugins.lsp_rust_cfg").setup()
-
-M.setup_lsp = function(attach, capabilities)
-   local lspconfig = require "lspconfig"
-   local servers = { "rustc-analyzer", "clangd" }
-
-   for _, lsp in ipairs(servers) do
-      lspconfig[lsp].setup {
-         on_attach = attach,
-         capabilities = capabilities,
-         flags = {
-            debounce_text_changes = 150,
-         },
-      }
-   end
-end
 
 return M
--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -1,4 +1,5 @@
 return {
   { "https://github.com/nvim-lua/lsp_extensions.nvim" },
   { "https://github.com/mg979/vim-visual-multi" },
+  { "https://github.com/simrat39/rust-tools.nvim" }
 }
