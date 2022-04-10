f57509404fcd4d3e5b9359275f903568d41ea37e
Defmc <defmc0101@gmail.com>
2022-04-10 16:36:32 -0400

    Update mdf

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -16,36 +16,26 @@ M.plugins = {
          setup_lspconf = "custom.plugins.lspconfig",
       },
    },
-   default_plugin_config_replace = {
-   },
+   default_plugin_config_replace = {},
    default_plugin_remove = { "extensions" },
 }
 
 M.mappings = {
-   -- terminal related mappings
    terminal = {
-      -- multiple mappings can be given for esc_termmode, esc_hide_termmode
-
-      -- get out of terminal mode
       esc_termmode = { "<esc>" },
+      esc_hide_termmode = { "<A-q>" },
+      pick_term = "<A>s",
 
-      -- get out of terminal mode and hide it
-      esc_hide_termmode = { "<A-h-i>" },
-      -- show & recover hidden terminal buffers in a telescope picker
-      pick_term = "<A-s>",
-
-      -- spawn a single terminal and toggle it
-      -- this just works like toggleterm kinda
+      -- split terminal
       new_horizontal = "<A-s>h",
       new_vertical = "<A-s>v",
-      new_float = "<A-s-w>",
+      new_float = "<A-f>",
 
-      -- spawn new terminals
+      -- creates new instance
       spawn_horizontal = "<A-h>",
       spawn_vertical = "<A-v>",
       spawn_window = "<A-w>",
    },
-
 }
 
 return M
--- .config/nvim/lua/custom/keymap.lua
+++ .config/nvim/lua/custom/keymap.lua
@@ -3,12 +3,11 @@ local M = {}
 M.setup = function()
    local map = require("core.utils").map
 
-   map("t", "<esc>", "<C-\\><C-n>")
    map("n", "Q", ":bd<CR>")
    map("n", "<C-S-Up>", ":m-2<CR>")
    map("n", "<C-S-Down>", ":m+1<CR>")
-   map("n", "<A-Left>", ":vertical resize -5<CR>")
-   map("n", "<A-Right>", ":vertical resize +5<CR>")
+   map("n", "<A-Left>", ":vertical resize +5<CR>")
+   map("n", "<A-Right>", ":vertical resize -5<CR>")
    map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
    map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
    map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
