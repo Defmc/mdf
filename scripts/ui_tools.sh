c9705ab9a6f8c81e0f044f785827b1b325424b67
Defmc <defmc0101@gmail.com>
2022-06-21 21:53:35 -0400

    update dotfiles

--- scripts/ui_tools.sh
+++ scripts/ui_tools.sh
@@ -4,7 +4,7 @@
 
 log_proc() {
     printf "%s$FG_RESET $2..." "[..]"
-    err=$(eval "$1" 2>&1 &>/dev/null)
+    err=$(eval "$1" 2>&1 &1>/dev/null)
     if [ $? -eq 0 ]; then
         printf "\r$FG_GREEN%s$RESET $2...\n" "[Ok]"
     else
