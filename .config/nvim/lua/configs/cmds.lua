9b63301c515d5074a0a112a4e694054d5ed32f63
Defmc <defmc0101@gmail.com>
2022-06-30 21:27:51 -0400

    update dotfiles

--- .config/nvim/lua/configs/cmds.lua
+++ .config/nvim/lua/configs/cmds.lua
@@ -3,10 +3,7 @@ local M = {}
 M.update = function()
 	vim.cmd([[PackerSync]])
 	vim.cmd([[TSUpdate]])
-	local servers = require("nvim-lsp-installer").get_installed_servers()
-	for _, svr in pairs(servers) do
-		vim.cmd("LspInstall --sync " .. svr.name)
-	end
+	vim.cmd([[LspInstallInfo]])
 end
 
 return M
