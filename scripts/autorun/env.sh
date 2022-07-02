9c01388e85b6851e1f828ebf7d1164ffff2bb3bc
Defmc <defmc0101@gmail.com>
2022-07-02 19:34:51 -0400

    update dotfiles

--- scripts/autorun/env.sh
+++ scripts/autorun/env.sh
@@ -2,8 +2,9 @@
 export EDITOR="nvim"
 export ZSH="$HOME/.oh-my-zsh"
 export ZSH_THEME="robbyrussell"
+export RUSTC_WRAPPER="~/.cargo/cachepot"
 
 export plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 
 . ~/.cargo/env
-. $ZSH/oh-my-zsh.sh
+. "$ZSH/oh-my-zsh.sh"
