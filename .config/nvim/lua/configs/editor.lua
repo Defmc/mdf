a8597f6bdda91be7a323cdb062632688f4f60eb8
Defmc <defmc0101@gmail.com>
2022-12-20 22:23:13 -0300

    update dotfiles

--- .config/nvim/lua/configs/editor.lua
+++ .config/nvim/lua/configs/editor.lua
@@ -10,5 +10,7 @@ require("vim").o.softtabstop = 4
 require("vim").o.shiftwidth = 4
 require("vim").o.expandtab = true
 require("vim").o.clipboard = "unnamedplus"
+require("vim").o.scrolloff = 4
+require("vim").o.incsearch = true
 
 require("configs.theme").setup()
