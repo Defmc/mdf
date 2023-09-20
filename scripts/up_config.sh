f1b792df0581466fd2fca572deaab29ddcd02b73
Defmc <defmc0101@gmail.com>
2023-09-20 15:51:47 -0400

    update dotfiles

--- scripts/up_config.sh
+++ scripts/up_config.sh
@@ -13,26 +13,27 @@ root_up_file() {
 }
 
 up_files() {
-  up_file . .Xresources
-  up_file . .xinitrc
-  up_file .icons .
-  up_file . .zshrc
-  up_file . .gtkrc-2.0
-  up_file .config gtk-3.0
-  up_file .config/picom picom.conf
-  up_file .config/nvim lua
-  up_file .config/nvim init.lua
-  up_file .config lemonbar
-  up_file .config polybar
-  up_file .config sxhkd
-  up_file .cargo config.toml
-  up_file .cargo env
-  up_file . scripts
-  up_file . .gdbinit
-  up_file .config bspwm
-  up_file img .
-  up_file .themes TokyoNight
-  root_up_file etc makepkg.conf
+  up_file . .Xresources &
+  up_file . .xinitrc &
+  up_file .icons . &
+  up_file . .zshrc &
+  up_file . .gtkrc-2.0 &
+  up_file .config gtk-3.0 &
+  up_file .config/picom picom.conf &
+  up_file .config/nvim lua &
+  up_file .config/nvim init.lua &
+  up_file .config lemonbar &
+  up_file .config polybar &
+  up_file .config sxhkd &
+  up_file .cargo config.toml &
+  up_file .cargo env &
+  up_file . scripts &
+  up_file . .gdbinit &
+  up_file .config bspwm &
+  up_file img . &
+  up_file .themes TokyoNight &
+  root_up_file etc makepkg.conf &
+  wait
 }
 
 up_git() {
