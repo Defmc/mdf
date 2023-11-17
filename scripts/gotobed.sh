00a3d760d20056d9c45d40eeb5988ef26880ce28
Defmc <defmc0101@gmail.com>
2023-11-17 15:05:00 -0400

    update dotfiles

--- scripts/gotobed.sh
+++ scripts/gotobed.sh
@@ -4,7 +4,7 @@ doas ~/scripts/set_time2online.sh
 cd ~/dev/unfoback
 deno run --allow-all app.js
 paru -Syyu --noconfirm
-omz update
+zsh -c "source ~/.zshrc && omz update"
 rustup update
 ~/scripts/up_config.sh
 ~/scripts/clear_cache.sh
