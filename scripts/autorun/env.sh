65dbb4d9c1301345937aa3b13e7a1307616ab2c0
Defmc <defmc0101@gmail.com>
2024-07-22 02:18:17 -0300

    update dotfiles

--- scripts/autorun/env.sh
+++ scripts/autorun/env.sh
@@ -3,7 +3,7 @@
 export ZSH="$HOME/.oh-my-zsh"
 export ZSH_THEME="lambda"
 
-export plugins=(git zsh-autosuggestions zsh-syntax-highlighting fzf-zsh-plugin)
+export plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 
 source "$HOME/.cargo/env"
 source "$ZSH/oh-my-zsh.sh"
