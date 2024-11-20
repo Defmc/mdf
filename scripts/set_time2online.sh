fc9aca390b3e2d078c195513bead88b9ab6e38a9
Defmc <defmc0101@gmail.com>
2024-11-20 16:26:42 -0300

    update dotfiles

--- scripts/set_time2online.sh
+++ scripts/set_time2online.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-date -s "$(python3 $(dirname "$0")/get_online_time.py)"
+baseurl="http://worldtimeapi.org/api/timezone"
+area="America"
+location="Sao_Paulo"
+region=""
+
+url="$baseurl/$area/$location"
+if [[ -z "$region" ]]; then
+    url="$url/$region"
+fi
+echo "$url"
+
+date=$(curl $url | sed -E 's/.*"datetime":"?([^,"]*)"?.*/\1/')
+echo "$date"
 hwclock --systohc
