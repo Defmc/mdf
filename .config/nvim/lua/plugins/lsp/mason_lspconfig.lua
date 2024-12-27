6cd9649d7183596a5a1a4e8b243cca99092f6dbb
Defmc <defmc0101@gmail.com>
2024-12-27 01:37:25 -0300

    update dotfiles

--- .config/nvim/lua/plugins/lsp/mason_lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/mason_lspconfig.lua
@@ -1,23 +1,49 @@
 return {
-	"williamboman/mason-lspconfig.nvim",
-	dependencies = {
-		"williamboman/mason.nvim",
-	},
-	after = "williamboman/mason.nvim",
-	config = function()
-		require("mason").setup()
-		require("mason-lspconfig").setup()
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+        "williamboman/mason.nvim",
+    },
+    after = "williamboman/mason.nvim",
+    config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup()
 
-		require("mason-lspconfig").setup_handlers({
-			-- The first entry (without a key) will be the default handler
-			-- and will be called for each installed server that doesn't have
-			-- a dedicated handler.
-			function(server_name) -- default handler (optional)
-				require("lspconfig")[server_name].setup({})
-				require("vim").lsp.inlay_hint.enable(true)
-			end,
-			-- Next, you can provide a dedicated handler for specific servers.
-			-- For example, a handler override for the `rust_analyzer`:
-		})
-	end,
+        local icons = require("lspkind").symbol_map
+        -- local signs = { "Error", "Warn", "Hint", "Info" }
+        -- for _, type in pairs(signs) do
+        --     local hl = "DiagnosticSign" .. type
+        --     require("vim").fn.sign_define(hl, { text = icons[type], texthl = hl, numhl = hl })
+        -- end
+        for _, diag in ipairs({ "Error", "Warn", "Info", "Hint" }) do
+            require("vim").fn.sign_define("DiagnosticSign" .. diag, {
+                text = "",
+                texthl = "DiagnosticSign" .. diag,
+                linehl = "",
+                numhl = "DiagnosticVirtualText" .. diag,
+            })
+        end
+
+        require("vim").diagnostic.config({
+            virtual_text = {
+                prefix = function(diagnostic)
+                    if diagnostic.severity == require("vim").diagnostic.severity.ERROR then
+                        return icons.Error
+                    elseif diagnostic.severity == require("vim").diagnostic.severity.WARN then
+                        return icons.Warn
+                    elseif diagnostic.severity == require("vim").diagnostic.severity.INFO then
+                        return icons.Info
+                    else
+                        return icons.Hint
+                    end
+                end
+            }
+        })
+
+        require("mason-lspconfig").setup_handlers({
+            function(server_name)
+                require("lspconfig")[server_name].setup({})
+                require("vim").lsp.inlay_hint.enable(true)
+            end,
+        })
+    end,
 }
