d62418472eb3a5677c7ea7287433c3b838a8cb5d
Defmc <defmc0101@gmail.com>
2025-05-28 22:07:51 -0300

    update dotfiles

--- .config/nvim/lua/plugins/lsp/mason_lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/mason_lspconfig.lua
@@ -50,11 +50,11 @@ return {
         })
 
         vim.lsp.inlay_hint.enable(true)
-        require("mason-lspconfig").setup_handlers({
-            function(server_name)
-                require("lspconfig")[server_name].setup({})
-            end,
-        })
+        -- require("mason-lspconfig").setup_handlers({
+        --     function(server_name)
+        --         require("lspconfig")[server_name].setup({})
+        --     end,
+        -- })
 
         vim.diagnostic.config({
             virtual_text = {
