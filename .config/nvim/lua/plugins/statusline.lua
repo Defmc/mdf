3f4d3b301b7ede043e23588b63d1f2ac3830e248
Defmc <defmc0101@gmail.com>
2026-05-06 10:41:45 -0300

    update dotfiles

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -8,7 +8,6 @@ return {
         -- Credit: glepnir
         local lualine = require('lualine')
 
-        -- Color table for highlights
         local color_theme = require("configs.theme").palette()
         local colors = {
             bg       = color_theme.dark0,
@@ -38,32 +37,24 @@ return {
             end,
         }
 
-        -- Config
         local config = {
             options = {
-                -- Disable sections and component separators
                 component_separators = '',
                 section_separators = '',
                 theme = {
-                    -- We are going to use lualine_c an lualine_x as left and
-                    -- right section. Both are highlighted by c theme .  So we
-                    -- are just setting default looks o statusline
                     normal = { c = { fg = colors.fg, bg = colors.bg } },
                     inactive = { c = { fg = colors.fg, bg = colors.bg } },
                 },
             },
             sections = {
-                -- these are to remove the defaults
                 lualine_a = {},
                 lualine_b = {},
-                lualine_y = {},
-                lualine_z = {},
-                -- These will be filled later
                 lualine_c = {},
                 lualine_x = {},
+                lualine_y = {},
+                lualine_z = {},
             },
             inactive_sections = {
-                -- these are to remove the defaults
                 lualine_a = {},
                 lualine_b = {},
                 lualine_y = {},
@@ -73,12 +64,10 @@ return {
             },
         }
 
-        -- Inserts a component in lualine_c at left section
         local function ins_left(component)
             table.insert(config.sections.lualine_c, component)
         end
 
-        -- Inserts a component in lualine_x at right section
         local function ins_right(component)
             table.insert(config.sections.lualine_x, component)
         end
@@ -149,11 +138,12 @@ return {
         local navic = require("nvim-navic")
         ins_left {
             function()
-                return "> " + navic.get_location({})
+                return "> " .. navic.get_location({})
             end,
             cond = function()
                 return navic.is_available()
-            end
+            end,
+            padding = { left = 0 }
         }
 
         -- Insert mid section. You can make any number of sections in neovim :)
@@ -229,7 +219,6 @@ return {
             padding = { left = 1 },
         }
 
-        -- Now don't forget to initialize lualine
         lualine.setup(config)
     end
 }
